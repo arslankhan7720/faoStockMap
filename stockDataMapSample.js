dfcaeca9a31feef31e608e54be7e643eb0936ed4
arsalan <arsalan.gohar@avest-group.com>
2021-05-31 13:57:30 +0500

    setup

---
+++ stockDataMapSample.js
@@ -0,0 +1,776 @@
+var stockMapJsonData = [
+  {
+    "id": 286,
+    "value": 56,
+    "priceType": "Increase",
+    "district": "BAGH"
+  },
+  {
+    "id": 287,
+    "value": 55,
+    "priceType": "Increase",
+    "district": "BHIMBER"
+  },
+  {
+    "id": 288,
+    "value": 54,
+    "priceType": "stable",
+    "district": "HATTIAN BALA"
+  },
+  {
+    "id": 289,
+    "value": 55,
+    "priceType": "stable",
+    "district": "HAVELI(KAHUTA)"
+  },
+  {
+    "id": 290,
+    "value": 52,
+    "priceType": "Decrease",
+    "district": "KOTLI"
+  },
+  {
+    "id": 291,
+    "value": 53,
+    "priceType": "Increase",
+    "district": "MIRPUR"
+  },
+  {
+    "id": 292,
+    "value": 54,
+    "priceType": "Increase",
+    "district": "MUZAFFARABAD"
+  },
+  {
+    "id": 293,
+    "value": 54,
+    "priceType": "stable",
+    "district": "NEELUM"
+  },
+  {
+    "id": 294,
+    "value": 54,
+    "priceType": "stable",
+    "district": "POONCH"
+  },
+  {
+    "id": 295,
+    "value": 54,
+    "priceType": "stable",
+    "district": "SUDHNOTI"
+  },
+  {
+    "id": 247,
+    "value": 56,
+    "priceType": "Decrease",
+    "district": "AWARAN"
+  },
+  {
+    "id": 248,
+    "value": 56,
+    "priceType": "stable",
+    "district": "BARKHAN"
+  },
+  {
+    "id": 249,
+    "value": 54,
+    "priceType": "Decrease",
+    "district": "CHAGAI"
+  },
+  {
+    "id": 250,
+    "value": 54,
+    "priceType": "stable",
+    "district": "DERA BUGTI"
+  },
+  {
+    "id": 251,
+    "value": 55,
+    "priceType": "stable",
+    "district": "GWADAR"
+  },
+  {
+    "id": 252,
+    "value": 55,
+    "priceType": "Decrease",
+    "district": "HARNAI"
+  },
+  {
+    "id": 253,
+    "value": 55,
+    "priceType": "stable",
+    "district": "JAFFARABAD"
+  },
+  {
+    "id": 254,
+    "value": 58,
+    "priceType": "stable",
+    "district": "SOHBATPUR"
+  },
+  {
+    "id": 255,
+    "value": 55,
+    "priceType": "Decrease",
+    "district": "JHAL MAGSI"
+  },
+  {
+    "id": 256,
+    "value": 54,
+    "priceType": "stable",
+    "district": "KACHHI"
+  },
+  {
+    "id": 257,
+    "value": 55,
+    "priceType": "stable",
+    "district": "KALAT"
+  },
+  {
+    "id": 258,
+    "value": 55,
+    "priceType": "stable",
+    "district": "KECH"
+  },
+  {
+    "id": 259,
+    "value": 56,
+    "priceType": "stable",
+    "district": "KHARAN"
+  },
+  {
+    "id": 260,
+    "value": 53,
+    "priceType": "stable",
+    "district": "KHUZDAR"
+  },
+  {
+    "id": 261,
+    "value": 54,
+    "priceType": "stable",
+    "district": "KILLA ABDULLAH"
+  },
+  {
+    "id": 262,
+    "value": 55,
+    "priceType": "stable",
+    "district": "KILLA SAIFULLAH"
+  },
+  {
+    "id": 263,
+    "value": 57,
+    "priceType": "stable",
+    "district": "KOHLU"
+  },
+  {
+    "id": 264,
+    "value": 60,
+    "priceType": "Increase",
+    "district": "LASBELA"
+  },
+  {
+    "id": 265,
+    "value": 57,
+    "priceType": "stable",
+    "district": "LORALAI"
+  },
+  {
+    "id": 266,
+    "value": 58,
+    "priceType": "stable",
+    "district": "MASTUNG"
+  },
+  {
+    "id": 267,
+    "value": 58,
+    "priceType": "stable",
+    "district": "MUSAKHEL"
+  },
+  {
+    "id": 268,
+    "value": 54,
+    "priceType": "stable",
+    "district": "NASIRABAD"
+  },
+  {
+    "id": 269,
+    "value": 55,
+    "priceType": "stable",
+    "district": "NUSHKI"
+  },
+  {
+    "id": 270,
+    "value": 56,
+    "priceType": "stable",
+    "district": "PANJGUR"
+  },
+  {
+    "id": 271,
+    "value": 55,
+    "priceType": "stable",
+    "district": "PISHIN"
+  },
+  {
+    "id": 272,
+    "value": 52,
+    "priceType": "Decrease",
+    "district": "QUETTA"
+  },
+  {
+    "id": 273,
+    "value": 58,
+    "priceType": "Decrease",
+    "district": "SHERANI"
+  },
+  {
+    "id": 274,
+    "value": 55,
+    "priceType": "stable",
+    "district": "LEHRI"
+  },
+  {
+    "id": 275,
+    "value": 55,
+    "priceType": "stable",
+    "district": "SIBI"
+  },
+  {
+    "id": 276,
+    "value": 56,
+    "priceType": "stable",
+    "district": "WASHUK"
+  },
+  {
+    "id": 277,
+    "value": 57,
+    "priceType": "stable",
+    "district": "ZHOB"
+  },
+  {
+    "id": 278,
+    "value": 56,
+    "priceType": "stable",
+    "district": "ZIARAT"
+  },
+  {
+    "id": 234,
+    "value": 56,
+    "priceType": "stable",
+    "district": "Bajaur"
+  },
+  {
+    "id": 237,
+    "value": 56,
+    "priceType": "stable",
+    "district": "FR Kohat"
+  },
+  {
+    "id": 239,
+    "value": 56,
+    "priceType": "stable",
+    "district": "FR Peshawar"
+  },
+  {
+    "id": 240,
+    "value": 52,
+    "priceType": "Decrease",
+    "district": "FR Tank"
+  },
+  {
+    "id": 241,
+    "value": 55,
+    "priceType": "stable",
+    "district": "Khyber"
+  },
+  {
+    "id": 242,
+    "value": 57,
+    "priceType": "stable",
+    "district": "Kurram"
+  },
+  {
+    "id": 243,
+    "value": 54,
+    "priceType": "stable",
+    "district": "Mohmand"
+  },
+  {
+    "id": 244,
+    "value": 56,
+    "priceType": "stable",
+    "district": "North Waziristan"
+  },
+  {
+    "id": 245,
+    "value": 56,
+    "priceType": "stable",
+    "district": "Orakzai"
+  },
+  {
+    "id": 246,
+    "value": 56,
+    "priceType": "stable",
+    "district": "South Waziristan"
+  },
+  {
+    "id": 279,
+    "value": 35,
+    "priceType": "stable",
+    "district": "ASTORE"
+  },
+  {
+    "id": 280,
+    "value": 38,
+    "priceType": "Increase",
+    "district": "SKARDU"
+  },
+  {
+    "id": 281,
+    "value": 36,
+    "priceType": "Increase",
+    "district": "DIAMIR"
+  },
+  {
+    "id": 282,
+    "value": 35,
+    "priceType": "Decrease",
+    "district": "GHANCHE"
+  },
+  {
+    "id": 283,
+    "value": 37,
+    "priceType": "stable",
+    "district": "GHIZER"
+  },
+  {
+    "id": 284,
+    "value": 34,
+    "priceType": "stable",
+    "district": "GILGIT"
+  },
+  {
+    "id": 285,
+    "value": 35,
+    "priceType": "stable",
+    "district": "HUNZA NAGAR"
+  },
+  {
+    "id": 148,
+    "value": 51,
+    "priceType": "stable",
+    "district": "ISLAMABAD"
+  },
+  {
+    "id": 209,
+    "value": 57,
+    "priceType": "stable",
+    "district": "ABBOTTABAD"
+  },
+  {
+    "id": 210,
+    "value": 56,
+    "priceType": "stable",
+    "district": "BANNU"
+  },
+  {
+    "id": 211,
+    "value": 57,
+    "priceType": "Decrease",
+    "district": "BATAGRAM"
+  },
+  {
+    "id": 212,
+    "value": 56,
+    "priceType": "Decrease",
+    "district": "BUNER"
+  },
+  {
+    "id": 213,
+    "value": 56,
+    "priceType": "stable",
+    "district": "CHARSADDA"
+  },
+  {
+    "id": 214,
+    "value": 59,
+    "priceType": "stable",
+    "district": "CHITRAL"
+  },
+  {
+    "id": 215,
+    "value": 53,
+    "priceType": "stable",
+    "district": "D. I. KHAN"
+  },
+  {
+    "id": 216,
+    "value": 54,
+    "priceType": "stable",
+    "district": "HANGU"
+  },
+  {
+    "id": 217,
+    "value": 52,
+    "priceType": "stable",
+    "district": "HARIPUR"
+  },
+  {
+    "id": 218,
+    "value": 53,
+    "priceType": "stable",
+    "district": "KARAK"
+  },
+  {
+    "id": 219,
+    "value": 52,
+    "priceType": "stable",
+    "district": "KOHAT"
+  },
+  {
+    "id": 220,
+    "value": 51,
+    "priceType": "Decrease",
+    "district": "KOHISTAN"
+  },
+  {
+    "id": 221,
+    "value": 52,
+    "priceType": "stable",
+    "district": "LAKKI MARWAT"
+  },
+  {
+    "id": 222,
+    "value": 54,
+    "priceType": "stable",
+    "district": "LOWER DIR"
+  },
+  {
+    "id": 223,
+    "value": 53,
+    "priceType": "stable",
+    "district": "MALAKAND P AREA"
+  },
+  {
+    "id": 224,
+    "value": 52,
+    "priceType": "stable",
+    "district": "MANSEHRA"
+  },
+  {
+    "id": 225,
+    "value": 52,
+    "priceType": "stable",
+    "district": "MARDAN"
+  },
+  {
+    "id": 226,
+    "value": 52,
+    "priceType": "stable",
+    "district": "NOWSHERA"
+  },
+  {
+    "id": 227,
+    "value": 52,
+    "priceType": "stable",
+    "district": "PESHAWAR"
+  },
+  {
+    "id": 228,
+    "value": 54,
+    "priceType": "stable",
+    "district": "SHANGLA"
+  },
+  {
+    "id": 229,
+    "value": 52,
+    "priceType": "stable",
+    "district": "SWABI"
+  },
+  {
+    "id": 230,
+    "value": 53,
+    "priceType": "stable",
+    "district": "SWAT"
+  },
+  {
+    "id": 231,
+    "value": 54,
+    "priceType": "stable",
+    "district": "TANK"
+  },
+  {
+    "id": 232,
+    "value": 57,
+    "priceType": "stable",
+    "district": "TOR GHAR"
+  },
+  {
+    "id": 233,
+    "value": 55,
+    "priceType": "stable",
+    "district": "UPPER DIR"
+  },
+  {
+    "id": 149,
+    "value": 41,
+    "priceType": "Decrease",
+    "district": "ATTOCK"
+  },
+  {
+    "id": 150,
+    "value": 41,
+    "priceType": "Decrease",
+    "district": "BAHAWALNAGAR"
+  },
+  {
+    "id": 151,
+    "value": 44,
+    "priceType": "Increase",
+    "district": "BAHAWALPUR"
+  },
+  {
+    "id": 152,
+    "value": 41,
+    "priceType": "Increase",
+    "district": "BHAKKAR"
+  },
+  {
+    "id": 153,
+    "value": 42,
+    "priceType": "Decrease",
+    "district": "CHAKWAL"
+  },
+  {
+    "id": 154,
+    "value": 44,
+    "priceType": "Increase",
+    "district": "CHINIOT"
+  },
+  {
+    "id": 155,
+    "value": 41,
+    "priceType": "Decrease",
+    "district": "DERA GHAZI KHAN"
+  },
+  {
+    "id": 156,
+    "value": 43,
+    "priceType": "Increase",
+    "district": "FAISALABAD"
+  },
+  {
+    "id": 157,
+    "value": 43,
+    "priceType": "stable",
+    "district": "GUJRANWALA"
+  },
+  {
+    "id": 158,
+    "value": 41,
+    "priceType": "Decrease",
+    "district": "GUJRAT"
+  },
+  {
+    "id": 159,
+    "value": 43,
+    "priceType": "stable",
+    "district": "HAFIZABAD"
+  },
+  {
+    "id": 160,
+    "value": 42,
+    "priceType": "stable",
+    "district": "JHANG"
+  },
+  {
+    "id": 161,
+    "value": 44,
+    "priceType": "Increase",
+    "district": "JHELUM"
+  },
+  {
+    "id": 162,
+    "value": 43,
+    "priceType": "Decrease",
+    "district": "KASUR"
+  },
+  {
+    "id": 163,
+    "value": 42,
+    "priceType": "Decrease",
+    "district": "KHANEWAL"
+  },
+  {
+    "id": 164,
+    "value": 42,
+    "priceType": "stable",
+    "district": "KHUSHAB"
+  },
+  {
+    "id": 165,
+    "value": 43,
+    "priceType": "Decrease",
+    "district": "LAHORE"
+  },
+  {
+    "id": 166,
+    "value": 42,
+    "priceType": "stable",
+    "district": "LAYYAH"
+  },
+  {
+    "id": 167,
+    "value": 41,
+    "priceType": "Decrease",
+    "district": "LODHRAN"
+  },
+  {
+    "id": 168,
+    "value": 42,
+    "priceType": "Decrease",
+    "district": "MANDI BAHAUDDIN"
+  },
+  {
+    "id": 169,
+    "value": 42,
+    "priceType": "Decrease",
+    "district": "MIANWALI"
+  },
+  {
+    "id": 170,
+    "value": 42,
+    "priceType": "stable",
+    "district": "MULTAN"
+  },
+  {
+    "id": 171,
+    "value": 42,
+    "priceType": "stable",
+    "district": "MUZAFFARGARH"
+  },
+  {
+    "id": 172,
+    "value": 41,
+    "priceType": "Decrease",
+    "district": "NANKANA SAHIB"
+  },
+  {
+    "id": 173,
+    "value": 42,
+    "priceType": "Decrease",
+    "district": "NAROWAL"
+  },
+  {
+    "id": 174,
+    "value": 42,
+    "priceType": "stable",
+    "district": "OKARA"
+  },
+  {
+    "id": 175,
+    "value": 41,
+    "priceType": "Decrease",
+    "district": "PAKPATTAN"
+  },
+  {
+    "id": 176,
+    "value": 42,
+    "priceType": "stable",
+    "district": "RAHIM YAR KHAN"
+  },
+  {
+    "id": 177,
+    "value": 41,
+    "priceType": "stable",
+    "district": "RAJANPUR"
+  },
+  {
+    "id": 178,
+    "value": 43,
+    "priceType": "Decrease",
+    "district": "RAWALPINDI"
+  },
+  {
+    "id": 179,
+    "value": 42,
+    "priceType": "stable",
+    "district": "SAHIWAL"
+  },
+  {
+    "id": 180,
+    "value": 41,
+    "priceType": "Decrease",
+    "district": "SARGODHA"
+  },
+  {
+    "id": 181,
+    "value": 43,
+    "priceType": "stable",
+    "district": "SHEIKHUPURA"
+  },
+  {
+    "id": 182,
+    "value": 41,
+    "priceType": "Decrease",
+    "district": "SIALKOT"
+  },
+  {
+    "id": 197,
+    "value": 52,
+    "priceType": "Decrease",
+    "district": "Mirpur Khas"
+  },
+  {
+    "id": 198,
+    "value": 53,
+    "priceType": "stable",
+    "district": "NAUSHAHRO FEROZE"
+  },
+  {
+    "id": 199,
+    "value": 53,
+    "priceType": "stable",
+    "district": "Sanghar"
+  },
+  {
+    "id": 200,
+    "value": 52,
+    "priceType": "stable",
+    "district": "SHAHEED BENAZIRABAD"
+  },
+  {
+    "id": 201,
+    "value": 53,
+    "priceType": "stable",
+    "district": "Shikarpur"
+  },
+  {
+    "id": 202,
+    "value": 52,
+    "priceType": "stable",
+    "district": "Sukkur"
+  },
+  {
+    "id": 235,
+    "value": 55,
+    "priceType": "stable",
+    "district": "FR Bannu"
+  },
+  {
+    "id": 236,
+    "value": 55,
+    "priceType": "stable",
+    "district": "FR D.I.Khan"
+  },
+  {
+    "id": 238,
+    "value": 56,
+    "priceType": "stable",
+    "district": "FR Lakki Marwat"
+  },
+  {
+    "id": 4067,
+    "value": 51,
+    "priceType": "Increase",
+    "district": "Karachi"
+  }
+]; 
